afc29240e5011aa115580a82076701c53376474d
elRatto21 <niklas031712@gmail.com>
2022-04-05 21:29:33 +0200

    Update Start.java

--- src/snakeMain/Start.java
+++ src/snakeMain/Start.java
@@ -16,12 +16,13 @@ import java.io.*;
 import javax.sound.sampled.AudioInputStream;
 import javax.sound.sampled.AudioSystem;
 import javax.sound.sampled.Clip;
+import javax.sound.sampled.FloatControl;
 import javax.sound.sampled.LineUnavailableException;
 import javax.sound.sampled.UnsupportedAudioFileException;
 
 /*
 * 
-* Snoke Version 0.2
+* Snoke Version 0.3
 *
 */
 
@@ -52,6 +53,7 @@ public class Start extends JFrame {
 	private int oldHigh;
 	private int scoreNow;
 	private int tempTempY;
+	private int volume = 10;
 	private int foodCount = 0;
 	private boolean done = false;
 	private int vier = 1;
@@ -62,6 +64,11 @@ public class Start extends JFrame {
 	private JLabel lGameover = new JLabel();
 	Random rnd = new Random();
 	private Canvas jMenue = new Canvas();
+	private JPanel jSetSettings = new JPanel();
+	private JSlider jSetDB = new JSlider();
+	private JLabel jSetDBText = new JLabel();
+	private JButton jSetHome = new JButton();
+	private JLabel jSetSetText = new JLabel();
 	private JLabel jHighscore = new JLabel();
 	private JLabel jScore = new JLabel();
 	private JLabel jEndscore = new JLabel();
@@ -114,6 +121,59 @@ public class Start extends JFrame {
 			}
 		});*/
 		
+		jSetDBText.setBounds(325, 169, 300, 75);
+		jSetDBText.setText("VOLUME");
+		jSetDBText.setForeground(Color.WHITE);
+		jSetDBText.setFont(new Font("Gill Sans Ultra Bold", Font.BOLD, 18));
+		jSetDBText.setVisible(false);
+		cp.add(jSetDBText);
+		
+		jSetSetText.setBounds(250, 69, 300, 75);
+		jSetSetText.setText("SETTINGS");
+		jSetSetText.setForeground(Color.WHITE);
+		jSetSetText.setFont(new Font("Gill Sans Ultra Bold", Font.BOLD, 40));
+		jSetSetText.setVisible(false);
+		cp.add(jSetSetText);
+		
+		jSetDB.setBounds(225, 200, 300, 100);
+		jSetDB.setVisible(false);
+		jSetDB.setMinorTickSpacing(1);
+	    jSetDB.setMajorTickSpacing(5);
+	    jSetDB.setPaintTicks(true);
+	    jSetDB.setPaintLabels(true);
+	    jSetDB.setMaximum(10);
+	    jSetDB.setMinimum(0);
+	    jSetDB.setFont(new Font("Gill Sans Ultra Bold", Font.PLAIN, 18));
+	    jSetDB.setBackground(new Color(0x404040));
+	    jSetDB.setForeground(Color.WHITE);
+	    jSetDB.setSnapToTicks(true);
+	    jSetDB.setOpaque(false);
+		cp.add(jSetDB);
+		
+		jSetHome.setBounds(225, 500, 300, 75);
+		jSetHome.setBackground(Color.GRAY);
+		jSetHome.setForeground(Color.WHITE);
+		jSetHome.setText("RETURN TO HOME");
+		jSetHome.setFont(new Font("Gill Sans Ultra Bold", Font.PLAIN, 18));
+		jSetHome.setVisible(false);
+		cp.add(jSetHome);
+		jSetHome.addActionListener(new ActionListener() {
+			public void actionPerformed(ActionEvent evt) {
+				try {
+					jSetHome_ActionPerformed(evt);
+				} catch (IOException e) {
+					// TODO Auto-generated catch block
+					e.printStackTrace();
+				}
+			}
+		});
+		
+		
+		jSetSettings.setBounds(0, 0, 764, 737);
+		jSetSettings.setBackground(Color.DARK_GRAY);
+		jSetSettings.setVisible(false);
+		cp.add(jSetSettings);
+		
 		jSnoke.setBounds(175, 50, 400, 100);
 		jSnoke.setText("Snoke");
 		jSnoke.setHorizontalAlignment(SwingConstants.CENTER);
@@ -133,13 +193,18 @@ public class Start extends JFrame {
 		cp.add(jStart);
 		jStart.addActionListener(new ActionListener() {
 			public void actionPerformed(ActionEvent evt) {
-				jStart_ActionPerformed(evt);
+				try {
+					jStart_ActionPerformed(evt);
+				} catch (IOException e) {
+					// TODO Auto-generated catch block
+					e.printStackTrace();
+				}
 			}
 		});
 		
 		jStats.setBounds(225, 300, 300, 50);
 		jStats.setBackground(Color.GRAY);
-		jStats.setForeground(Color.RED);
+		jStats.setForeground(Color.WHITE);
 		jStats.setFont(new Font("Gill Sans Ultra Bold", Font.PLAIN, 18));
 		jStats.setText("STATS");
 		jStats.setVisible(true);
@@ -147,15 +212,25 @@ public class Start extends JFrame {
 		
 		jSettings.setBounds(225, 400, 300, 50);
 		jSettings.setBackground(Color.GRAY);
-		jSettings.setForeground(Color.RED);
+		jSettings.setForeground(Color.WHITE);
 		jSettings.setFont(new Font("Gill Sans Ultra Bold", Font.PLAIN, 18));
 		jSettings.setText("SETTINGS");
 		jSettings.setVisible(true);
 		cp.add(jSettings);
+		jSettings.addActionListener(new ActionListener() {
+			public void actionPerformed(ActionEvent evt) {
+				try {
+					jSettings_ActionPerformed(evt);
+				} catch (IOException e) {
+					// TODO Auto-generated catch block
+					e.printStackTrace();
+				}
+			}
+		});
 		
 		jQuit.setBounds(225, 500, 300, 50);
 		jQuit.setBackground(Color.GRAY);
-		jQuit.setForeground(Color.WHITE);
+		jQuit.setForeground(Color.RED);
 		jQuit.setFont(new Font("Gill Sans Ultra Bold", Font.PLAIN, 18));
 		jQuit.setText("QUIT");
 		jQuit.setVisible(true);
@@ -414,6 +489,7 @@ public class Start extends JFrame {
 			}
 			clip.loop(Clip.LOOP_CONTINUOUSLY);
 			clip.setFramePosition(0);
+			clipVolume(1);
 			clip.start();
 			gameO = 1;
 		} else if(gameO == 1) {
@@ -430,6 +506,7 @@ public class Start extends JFrame {
 				clip2.open(audioIn2);
 			}
 			clip2.setFramePosition(0);
+			clipVolume(2);
 			clip2.start();
 			gameO = 2;
 			if(done == false) {
@@ -483,7 +560,7 @@ public class Start extends JFrame {
 		System.exit(EXIT_ON_CLOSE);
 	}
 	
-	public void jStart_ActionPerformed(ActionEvent evt) {
+	public void jStart_ActionPerformed(ActionEvent evt) throws IOException {
 		jStart.setVisible(false);
 		jSettings.setVisible(false);
 		jStats.setVisible(false);
@@ -491,7 +568,36 @@ public class Start extends JFrame {
 		jSnoke.setVisible(false);
 		jMenue.setVisible(false);
 		jHome.setVisible(false);
+		readVolume();
 		starteDiese();
+	}
+	
+	public void jSettings_ActionPerformed(ActionEvent evt) throws IOException {
+		jStart.setVisible(false);
+		jSettings.setVisible(false);
+		jStats.setVisible(false);
+		jQuit.setVisible(false);
+		jSnoke.setVisible(false);
+		jMenue.setVisible(false);
+		jHome.setVisible(false);
+		settings();
+	}
+	
+	public void jSetHome_ActionPerformed(ActionEvent evt) throws IOException {
+		jSetSettings.setVisible(false);
+		jSetDB.setVisible(false);
+		jSetDBText.setVisible(false);
+		jSetHome.setVisible(false);
+		jSetSetText.setVisible(false);
+		volume = jSetDB.getValue();
+		writeVolume();
+		jStart.setVisible(true);
+		jSettings.setVisible(true);
+		jStats.setVisible(true);
+		jQuit.setVisible(true);
+		jSnoke.setVisible(true);
+		jMenue.setVisible(true);
+		jHome.setVisible(true);
 	}
 	
 	public void jHome_ActionPerformed(ActionEvent evt) throws NumberFormatException, IOException {
@@ -549,6 +655,137 @@ public class Start extends JFrame {
 	
 	public int getDir() {
 		return this.dir;
+	}
+	
+	public void settings() throws IOException {
+		
+		readVolume();
+		jSetDB.setValue(volume);
+		jSetSettings.setVisible(true);
+		jSetDB.setVisible(true);
+		jSetHome.setVisible(true);
+		jSetSetText.setVisible(true);
+		jSetDBText.setVisible(true);
+		
+	}
+	
+	public void clipVolume(int clipNr) {
+		
+		
+		if(clipNr == 1) {
+			FloatControl gainControl = (FloatControl) clip.getControl(FloatControl.Type.MASTER_GAIN);
+			switch(volume) {
+			case 0:
+				gainControl.setValue(-80f);
+				break;
+			case 1:
+				gainControl.setValue(-63f);
+				break;
+			case 2:
+				gainControl.setValue(-56f);
+				break;
+			case 3:
+				gainControl.setValue(-49f);
+				break;
+			case 4:
+				gainControl.setValue(-42f);
+				break;
+			case 5:
+				gainControl.setValue(-35f);
+				break;
+			case 6:
+				gainControl.setValue(-28f);
+				break;
+			case 7:
+				gainControl.setValue(-21f);
+				break;
+			case 8:
+				gainControl.setValue(-14f);
+				break;
+			case 9:
+				gainControl.setValue(-7f);
+				break;
+			case 10:
+				gainControl.setValue(0);
+				break;
+			}
+		} else {
+			FloatControl gainControl2 = (FloatControl) clip2.getControl(FloatControl.Type.MASTER_GAIN);
+			switch(volume) {
+			case 0:
+				gainControl2.setValue(-80f);
+				break;
+			case 1:
+				gainControl2.setValue(-63f);
+				break;
+			case 2:
+				gainControl2.setValue(-56f);
+				break;
+			case 3:
+				gainControl2.setValue(-49f);
+				break;
+			case 4:
+				gainControl2.setValue(-42f);
+				break;
+			case 5:
+				gainControl2.setValue(-35f);
+				break;
+			case 6:
+				gainControl2.setValue(-28f);
+				break;
+			case 7:
+				gainControl2.setValue(-21f);
+				break;
+			case 8:
+				gainControl2.setValue(-14f);
+				break;
+			case 9:
+				gainControl2.setValue(-7f);
+				break;
+			case 10:
+				gainControl2.setValue(0);
+				break;
+			}
+			
+		}
+		
+	}
+	
+	public void writeVolume() throws IOException {
+		File vol = new File("vol.txt");
+		if(vol.createNewFile()) {
+			Writer fwNew = new FileWriter(vol);
+			BufferedWriter bwNew = new BufferedWriter(fwNew);
+			bwNew.write("10");
+			bwNew.flush();
+			bwNew.close();
+			fwNew.close();
+		}
+		Writer fw = new FileWriter(vol);
+		BufferedWriter bw = new BufferedWriter(fw);
+		
+		bw.write(String.valueOf(volume));
+		bw.flush();
+		bw.close();
+		fw.close();
+	}
+	
+	public void readVolume() throws IOException {
+		File vol = new File("vol.txt");
+		if(vol.createNewFile()) {
+			System.out.println("yikes");
+			Writer fwNew = new FileWriter(vol);
+			BufferedWriter bwNew = new BufferedWriter(fwNew);
+			bwNew.write("10");
+			bwNew.flush();
+			bwNew.close();
+			fwNew.close();
+		}
+		Reader fr = new FileReader(vol);
+		BufferedReader br = new BufferedReader(fr);
+		
+		String volStr = br.readLine();
+		volume = Integer.parseInt(volStr);
 	}
 	
 	private class MoveAction extends AbstractAction{
